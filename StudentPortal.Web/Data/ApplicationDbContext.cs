b33410018fd71b51a3a54a5b4e42de0065e54fa1
Nimesha Matteo Perera <mattperdevstuff@gmail.com>
2024-04-12 00:06:41 +0530

    Add project files.

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Db Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //creates students Db
+        public DbSet<Student> Students { get; set; }
+    }
+}
